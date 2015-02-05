d7483219fd3235b5612b29557bb46e28e28cf40b
K-langoSoft <fabio4126@itelefonica.com.br>
2015-02-05 19:59:31 -0200

    Primeira classe

---
+++ Calculadora.py
@@ -0,0 +1,23 @@
+#--*-- encoding utf-8 --*--
+class Escolha():
+    def escolha(self):
+        print('Coloque a sua operacao desejada: ')
+        print('[ + ] - Soma')
+        print('[ - ] - Subtracao')
+        print('[ * ] - Multiplicacao')
+        print('[ / ] - Divisao')
+        op = raw_input('= ')
+
+class Operacoes():
+    def Soma(self):
+        pass
+    def Subtracao(self):
+        pass
+    def Multiplicacao(self):
+        pass
+    def Divisao(self):
+        pass
+        n1 = raw_input(': ')
+e1 = Escolha()
+e1.escolha()
+
