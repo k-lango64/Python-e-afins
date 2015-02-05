b68775373504f05c966fc96ab1bade5feeac829f
K-langoSoft <fabio4126@itelefonica.com.br>
2015-02-05 19:59:31 -0200

    Introduzindo a primeira classe

---
+++ Calculadora.py
@@ -0,0 +1,11 @@
+#--*-- encoding utf-8 --*--
+class Escolha():
+    def escolha(self):
+        print('Coloque a sua operacao desejada: ')
+        print('[ + ] - Soma')
+        print('[ - ] - Subtracao')
+        print('[ * ] - Multiplicacao')
+        print('[ / ] - Divisao')
+        n1 = raw_input(': ')
+e1 = Escolha()
+e1.escolha()
